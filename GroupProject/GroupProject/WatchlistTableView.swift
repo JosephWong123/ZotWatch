025674f9e10ca21331530de5966cc4cbe5c08fd4
Garry Fanata <garryfanata@gmail.com>
2019-06-06 23:03:12 -0700

    +Watchlist Table view

---
+++ GroupProject/GroupProject/WatchlistTableView.swift
@@ -0,0 +1,21 @@
+//
+//  WaitlistTableView.swift
+//  GroupProject
+//
+//  Created by Garry Fanata on 6/6/19.
+//  Copyright © 2019 Kinaar Desai. All rights reserved.
+//
+
+import UIKit
+
+class WatchlistTableView: UITableView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
