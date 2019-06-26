33b31f7a8e6edac801dfed4169e0b2a58f2bd8ea
Joseph Wong <josephw0823@gmail.com>
2019-06-25 18:42:18 -0700

    More backend fixes
    -Fixed classes that have two different course codes (different departments sharing a class)
    -Fixed the glitch where the app crashes for the writing department

--- GroupProject/GroupProject/Request Handler/GetCourseInfo.swift
+++ GroupProject/GroupProject/Request Handler/GetCourseInfo.swift
@@ -66,11 +66,17 @@ class GetCourseInfo {
                         }
                         let status = elements[elements.count-1]
                         var seatString : String
-                        let seatsRange = line.range(of: #"(?<!\d)(\d{1,3}\s+){2}(\d{1,3}|n\/a)(\s+\d{1,3}){2}"#, options: .regularExpression)
+                        let seatsRange = line.range(of: #"(?<!\d)(\d{1,3}\s+)((\d{1,3}\s+)|(\d{1,3}\/\d{1,3}\s+))(\d{1,3}|n\/a)\s+(\d{1,3}\s+){2}"#, options: .regularExpression)
                         seatString = String(line[seatsRange!]).trimmingCharacters(in: .whitespaces)
                         
                         var seatArray : [String] = seatString.components(separatedBy: .whitespaces)
                         let maxSeats = Int(seatArray[0]) ?? 0
+                        if seatArray[1].count > 3 {
+                            let index = seatArray[1].index(of: "/")
+                            seatArray[1] = String(seatArray[1].substring(from: index!))
+                            let offset1 = seatArray[1].index(seatArray[1].startIndex, offsetBy: 1)
+                            seatArray[1] = String(seatArray[1].substring(from: offset1))
+                        }
                         let seatsTaken = Int(seatArray[1]) ?? 0
                         let seatsReserved = Int(seatArray[seatArray.count-1]) ?? 0
                         sections.append(CourseSection(courseCode: code, type: type, section: section, instructor: instructor, days: days, time: time, place: place, status: status, maxSeats: maxSeats, seatsTaken: seatsTaken, seatsReserved: seatsReserved))
@@ -112,7 +118,7 @@ class GetCourseInfo {
                 for var line in lines {
                     line = line.trimmingCharacters(in: .whitespacesAndNewlines)
                     var elements : [String] = line.components(separatedBy: "  ")
-                    if (elements[0].lowercased() == dept.lowercased()) {
+                    if (elements[0].lowercased() == dept.lowercased()) && (elements.count >= 3) {
                         elements = elements.filter { $0 != "" }
                         courses.append(Course(dept: elements[0], courseNum: elements[1], courseTitle: elements[2]))
                     }
